103e1b83148ecce0076e96dd21e22da668d21df3
Ross Cooperman <ross@csinitiative.com>
2008-07-29 16:10:20 +0000

    Implemented basic migrations: - migration files go in db/migrations and names should be of the form <yyyymmddhhmmss>_SomeDescriptiveCamelCasedName.php - migration files need to contain a class that matches the filename:   - the class for <yyyymmddhhmmss>_SomeDescriptiveCamelCasedName.php would look like "class SomeDescriptiveCamelCasedName extends Migration {}" - migration class needs to implement an up() and a down() method   - up() is called when migrating to or past this version (version = the first numeric part of the file, <yyyymmddhhmmss> in the above example)   - down() is called when migrating past or to the version just prior to the migration     - by way of example, say you have migrations with versions 20080729000000, 20080729000001, and 20080729000002:       - if your schema is at version 20080729000002 and you migrate to version 20080729000000, the down methods in version ...002 and ...001 will be run       - the logic here is that you are migrating to a version which corresponds with the result of migrating up to that version hence running that versions down() method would not be appropriate - createTable() and dropTable() are implemented and working - script/migrate.php is able to migrate up to the highest version or to any specific version (up or down) - should work for mysqli and pdo_mysql zend_db adapters; other adapters could be added fairly simply - syntax for createTable needs to be documented (dropTable is simple, just pass the name of the table)
    This will all be fully documented on the wiki
    
    git-svn-id: https://qframe.csinitiative.net/svn/qframe/trunk@41 15cf7570-8350-0410-9b3b-ce1bb776223b

---
+++ library/Migration/Adapter/Mysql.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of the CSI RegQ.
+ *
+ * The CSI RegQ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CSI RegQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   Migration
+ * @package    Migration
+ * @copyright  Copyright (c) 2007 Collaborative Software Initiative (CSI)
+ * @license    http://www.gnu.org/licenses/   GNU General Public License v3
+ */
+
+
+/**
+ * @category   Migration
+ * @package    Migration
+ * @copyright  Copyright (c) 2007 Collaborative Software Initiative (CSI)
+ * @license    http://www.gnu.org/licenses/   GNU General Public License v3
+ */
+abstract class Migration_Adapter_Mysql extends Migration_Adapter {
+
+  /**
+   * Maps abtract types to the correct MySQL type
+   *
+   * @param  string  type to be mapped
+   * @param  integer (optional) size
+   * @return string
+   */
+  public function mapType($type, $size = null) {
+    switch(strtolower($type)) {
+      case 'integer':
+        return 'INT';
+      case 'decimal':
+      case 'float':
+      case 'datetime':
+      case 'date':
+      case 'timestamp':
+      case 'time':
+      case 'text':
+        return strtoupper($type);
+      case 'string':
+        $size = ($size) ? $size : 255;
+        return "VARCHAR({$size})";
+      case 'binary':
+        return 'BLOB';
+      case 'boolean':
+        return 'TINYINT';
+      default:
+        throw new Exception("Unknown abstract type '{$type}'");
+    }
+  }
+  
+  /**
+   * Convert an array of column elements into the text to create that column
+   *
+   * @param  array column parameters
+   * @return string
+   */
+  public function generateColumn($column, array $options) {
+    if(count($column) < 2)
+      throw new Exception("Invalid column specifier " . '[ "' . implode('","', $column) . '" ]');
+      
+    $name = $column[0];
+    $type = $column[1];
+    $limit = (isset($column[2])) ? $column[2] : null;
+    $default = (isset($column[3])) ? $this->dbAdapter->quote($column[3]) : null;
+    $null = (isset($column[4]) && !$column[4]) ? false : true;
+    
+    $column = "  {$this->dbAdapter->quoteIdentifier($name)} {$this->mapType($type, $limit)}";
+    if($default) $column .= " DEFAULT {$default}";
+    if(!$null) $column .= ' NOT NULL';
+    if($options['primary'] === $name && (!isset($options['auto']) || $options['auto'])) {
+      $column .= ' AUTO_INCREMENT';
+    }
+    return $column;
+  }
+  
+  /**
+   * Create a new table
+   *
+   * @param  string table name
+   * @param  array  options for creating the table
+   * @param  array  array of columns (which is each an array)
+   * @return boolean
+   */
+  public function createTable($name, array $options, array $columns) {
+    // check for a 'primary' option and act appropriately based on what its value is
+    if(!isset($options['primary'])) {
+      array_unshift($columns, array('id', 'integer', null, 0, false));
+      $options['primary'] = 'id';
+    }
+    
+    // check for a valid primary key
+    if(!$this->validPrimaryKey($options['primary'], $columns)) {
+      die("Specified primary key column does not exist [{$options['primary']}]\n\n");
+    }
+    
+    // build up the column definitions 
+    $query = "CREATE TABLE {$this->dbAdapter->quoteIdentifier($name)} (\n";
+    foreach($columns as $column) {
+      $columnStrings[] = $this->generateColumn($column, $options);
+    }
+    
+    // add a "column definition" for a primary key if necessary
+    if($options['primary']) {
+      $columnStrings[] = "  PRIMARY KEY({$this->dbAdapter->quoteIdentifier($options['primary'])})";
+    }
+    
+    // explode column definitions into SQL, tack on the last little bit, and run the query
+    $query .= implode(",\n", $columnStrings);
+    $query .= "\n) ENGINE InnoDB DEFAULT CHARSET='utf8'";
+    return $this->dbAdapter->query($query);
+  }
+  
+  /**
+   * Check to make sure that the passed in primary key is a valid column
+   *
+   * @param  string primary key name
+   * @param  array  column list
+   * @return boolean
+   */
+  protected final function validPrimaryKey($key, $columns) {
+    foreach($columns as $column) if($column[0] === $key) return true;
+    return false;
+  }
+  
+  /**
+   * Drop a table
+   *
+   * @param  string table name
+   * @return boolean
+   */
+  public function dropTable($table) {
+    $query = "DROP TABLE {$this->dbAdapter->quoteIdentifier($table)}";
+    return $this->dbAdapter->query($query);
+  }
+}
