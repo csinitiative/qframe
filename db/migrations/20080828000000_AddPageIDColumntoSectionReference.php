d5e5c48cda4d5a84b2907f24197e0132d84b0cd5
Ross Cooperman <ross@csinitiative.com>
2008-09-17 04:59:07 +0000

    Modified migration to update all existing rows in section_reference with the proper pageID
    git-svn-id: https://qframe.csinitiative.net/svn/qframe/trunk@86 15cf7570-8350-0410-9b3b-ce1bb776223b

--- db/migrations/20080828000000_AddPageIDColumntoSectionReference.php
+++ db/migrations/20080828000000_AddPageIDColumntoSectionReference.php
@@ -2,7 +2,17 @@
 class AddPageIDColumnToSectionReference extends Migration {
 
   public function up() {
+    // add the column
     $this->addColumn('section_reference', 'pageID', 'integer', array('limit' => 20));
+    
+    // update the column for existing rows
+    $adapter = Zend_Db_Table_Abstract::getDefaultAdapter();
+    $statement = $adapter->select()->from('section')->query();
+    while($section = $statement->fetchObject()) {
+      $newValues = array('pageID' => $section->pageID);
+      $where = "sectionID={$section->sectionID}";
+      $adapter->update('section_reference', $newValues, $where);
+    }
   }
   
   public function down() {
